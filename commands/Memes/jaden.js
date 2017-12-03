e524525bcdc9ea17c9ffc8292b70802a160bc52c
Michael Louro <31722675+malouro@users.noreply.github.com>
2017-12-03 10:57:41 -0500

    Jaden Quotes
    New Command: Jaden Quotes

---
+++ commands/Memes/jaden.js
@@ -0,0 +1,29 @@
+// =====================================================================================
+//                               ! jaden command
+// =====================================================================================
+
+const fs = require('fs');
+const moment = require('moment');
+const settings = require('../../settings');
+
+exports.run = (bot, message, args) => {
+    var conf = JSON.parse(fs.readFileSync('./config/quotes/jaden.json', 'utf8'));
+    var rng = Math.floor(Math.random() * conf.tweets.size);
+    message.channel.send(`${conf.baseURL}${conf.tweets[rng].url}`);
+    console.log(`[${moment().format(settings.timeFormat)}] User ${message.author.username} Is Being Enlightened`);    
+}
+
+exports.conf = {
+    enabled: true,
+    visible: true,
+    guildOnly: true,
+    textChannelOnly: false,
+    aliases: [],
+    permLevel: 0
+};
+
+exports.help = {
+    name: 'jaden',
+    description: 'Get A Random Quote From The Aristotle Of Our Generation',
+    usage: 'jaden'
+};
