1757eee8cdbcdbf0f30b2ad4786277c7c8b2c54b
Michael Louro <31722675+malouro@users.noreply.github.com>
2018-02-01 21:48:43 -0500

    !invite update
    - !invite now generates an invite link dynamically, rather than a static URL
    - Recategorized !invite as a Debug & Support command
    - Added new streamLink prop to settings.json

--- commands/Debug & Support/invite.js
+++ commands/Debug & Support/invite.js
@@ -9,8 +9,6 @@
 const moment = require('moment');
 const settings = require('../../settings.json');
 
-const url = '<https://discordapp.com/oauth2/authorize?client_id=321818035193118722&scope=bot&permissions=523762774>';
-
 exports.help = {
   name: 'invite',
   description: `Posts invite link to invite ${settings.botNameProper} to your server`,
@@ -27,7 +25,10 @@ exports.conf = {
 };
 
 exports.run = (bot, message) => {
-  message.channel.send(`If you wish to invite <@${bot.user.id}> to your Discord server, use the link below and choose a server from the dropdown list. You must have the **Manage Server** permission on the server in order for it to show up on the list, and for the invite to work.\n\nYou may add or remove permissions as you please, but keep in mind that it might break current or future features.\n\n${url}`);
+  message.channel.send(`If you wish to invite <@${bot.user.id}> to your Discord server, use the link below and choose a server from the dropdown list.` +
+  'You must have the **Manage Server** permission on the server in order for it to show up on the list, and for the invite to work.\n\n' +
+  'You may add or remove permissions as you please, but keep in mind that it might break current or future features.\n\n' +
+  `${bot.generateInvite(523762774)}`);
 
   console.log(`[${moment().format(settings.timeFormat)}] User ${message.author.username} asked for an invite link`);
 };
