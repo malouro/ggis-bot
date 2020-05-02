d5b19a73c26e7a917de674c6fffdc7a223e55965
Michael Louro <michael.aa.louro@gmail.com>
2020-05-01 22:02:02 -0400

    [Test] Add testing

--- test/unit/handlers.test.js
+++ test/unit/handlers.test.js
@@ -1,20 +1,85 @@
-const { expect } = require('chai');
-const sinon = require('sinon');
+const fs = require('fs');
+const waitForExpect = require('wait-for-expect');
+const mockConsole = require('jest-mock-console').default;
+const { Collection } = require('discord.js');
+const Setup = require('../../handlers/Setup');
+const { platforms } = require('../../config/lfg/platforms.json');
 
-const EventLoader = require('../../handlers/EventLoader');
-
-const testBot = {
-  on: sinon.spy(),
+const settings = {
+  botName: 'ggis',
+  botNameProper: 'Ggis',
+  token: 'fake-token',
+  prefix: '!',
+  commandGroups: [
+    {
+      code: [
+        'debug',
+        'support',
+      ],
+      name: 'Debug & Support',
+      description: 'Help menus & debug',
+    },
+    {
+      code: [
+        'useful',
+      ],
+      name: 'Useful',
+      description: 'Commands that do useful things',
+    },
+    {
+      code: [
+        'random',
+        'rng',
+      ],
+      name: 'Random',
+      description: 'Random, fun commands',
+    },
+    {
+      code: [
+        'memes',
+      ],
+      name: 'Memes',
+      description: 'Memes & dreams',
+    },
+  ],
 };
-const settings = {};
+
+const bot = {
+  on: jest.fn(),
+};
+
+jest.mock('twitchps');
 
 describe('Handlers', () => {
-  describe('Event Loader', () => {
-    it('loads necessary event handlers', () => {
-      EventLoader(testBot, settings);
+  const restoreConsole = mockConsole();
 
-      expect(testBot.on.callCount).to.equal(11);
-      expect(testBot.on.calledOnceWith('ready')).to.equal(true);
+  /* Wait for setup to be done */
+  beforeAll(async () => {
+    const numberOfExpectedCommands = 29;
+    const lfgGames = fs.readdirSync('./config/lfg/default');
+    await Setup(bot, settings);
+
+    await waitForExpect(() => {
+      expect(bot.games.size).toBe(lfgGames.length);
+      expect(bot.commands.size).toBe(numberOfExpectedCommands);
+      expect(bot.platforms.size).toBe(platforms.length);
+    }, 10000);
+  }, 20000);
+
+  afterAll(() => restoreConsole());
+
+  describe('Bot setup', () => {
+    test('initializes necessary bot properties', () => {
+      expect(bot.commandGroups).toBeInstanceOf(Collection);
+      expect(bot.commandGroupCategories).toBeInstanceOf(Collection);
+      expect(bot.commands).toBeInstanceOf(Collection);
+      expect(bot.aliases).toBeInstanceOf(Collection);
+    });
+  });
+
+  describe('Event Loader', () => {
+    test('loads event handlers', () => {
+      expect(bot.on).toHaveBeenCalledTimes(11);
     });
   });
 });
