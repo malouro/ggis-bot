80445ddd4c18d523ba7989ad28492c735c2efcad
Michael Louro <michael.aa.louro@gmail.com>
2020-10-05 00:10:12 -0400

    [Test] Add test data for settings

---
+++ test/data/index.js
@@ -0,0 +1,46 @@
+/** @type {import('../../commands/Useful/settings').ServerSettings} */
+exports.serverSettings = {
+  test: {
+    'range 0-10': {
+      type: 'range',
+      min: 0,
+      max: 10,
+      default: 0,
+      description: 'Some number within this range',
+    },
+    number: {
+      type: 'number',
+    },
+    integer: {
+      type: 'integer',
+    },
+    boolean: {
+      type: 'boolean',
+    },
+    string: {
+      type: 'string',
+    },
+    arrayOfStrings: {
+      type: 'array',
+      innerType: 'string',
+      description: 'An array of strings',
+    },
+    arrayOfNumbers: {
+      type: 'array',
+      innerType: 'number',
+      description: 'An array of numbers',
+    },
+    arrayOfRanges: {
+      type: 'array',
+      innerType: 'range',
+      min: 1,
+      max: 10,
+      description: 'An array of ranges',
+    },
+    arrayOfBooleans: {
+      type: 'array',
+      innerType: 'boolean',
+      description: 'An array of booleans',
+    },
+  },
+};
