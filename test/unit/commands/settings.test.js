4ed33401876f2404da27f427fffde380258c4996
Michael Louro <michael.aa.louro@gmail.com>
2020-10-04 23:56:25 -0400

    [Settings] Add array type settings

--- test/unit/commands/settings.test.js
+++ test/unit/commands/settings.test.js
@@ -3,23 +3,24 @@ const path = require('path');
 const waitForExpect = require('wait-for-expect');
 const settingsCommand = require('../../../commands/Useful/settings');
 const { MOCK_BOT, MOCK_GUILD_ID, makeMockMessage } = require('../../testHelpers');
+const { serverSettings: testServerSettings } = require('../../data');
 
 const pathToMockGuildConfig = path.resolve(__dirname, `../../../config/guilds/${MOCK_GUILD_ID}.json`);
 
-describe('Settings command', () => {
-  afterEach(async () => {
-    const guildConfigExists = () => fs.existsSync(pathToMockGuildConfig);
+const guildConfigExists = () => fs.existsSync(pathToMockGuildConfig);
 
-    MOCK_BOT.clearMocks();
+beforeEach(async () => {
+  if (guildConfigExists()) {
+    fs.unlinkSync(pathToMockGuildConfig);
+    await waitForExpect(() => {
+      expect(guildConfigExists()).toBe(false);
+    });
+  }
+});
 
-    if (guildConfigExists()) {
-      fs.unlinkSync(pathToMockGuildConfig);
-      await waitForExpect(() => {
-        expect(guildConfigExists()).toBe(false);
-      });
-    }
-  });
+afterEach(() => MOCK_BOT.clearMocks());
 
+describe('Settings Command', () => {
   test('shows full list of settings that can be configured/overridden', async () => {
     const args = ['!settings', 'list'];
     await settingsCommand.run(MOCK_BOT, makeMockMessage(args), args);
@@ -41,9 +42,49 @@ describe('Settings command', () => {
     const argsToChangePrefix = ['!settings', 'bot', 'prefix', newPrefix];
     await settingsCommand.run(MOCK_BOT, makeMockMessage(argsToChangePrefix), argsToChangePrefix);
 
+    await waitForExpect(() => {
+      expect(
+        fs.readFileSync(`./config/guilds/${MOCK_GUILD_ID}.json`, 'utf-8'),
+      ).toMatch(new RegExp(`"prefix":\\s*"${newPrefix}"`));
+    });
+
     /* Check prefix with `!settings show` */
-    const argsToCheckPrefix = ['!settings', 'show'];
+    const argsToCheckPrefix = ['@@settings', 'show'];
     await settingsCommand.run(MOCK_BOT, makeMockMessage(argsToCheckPrefix), argsToCheckPrefix);
     expect(MOCK_BOT.message.reply).toHaveBeenCalledWith(expect.stringContaining(`"bot": { "prefix": "${newPrefix}" }`));
+  });
+
+  test.each([
+    ['string', ['test-value'], '"test-value"'],
+    ['number', ['10'], 10],
+    ['boolean', ['true'], true],
+    ['boolean', ['false'], false],
+    ['arrayOfStrings', ['one', 'two'], '\\[\\s*"one",\\s*"two"\\s*\\]'],
+    ['arrayOfNumbers', ['1', '2', '3'], '\\[\\s*1,\\s*2,\\s*3\\s*\\]'],
+    ['arrayOfBooleans', ['true', 'false'], '\\[\\s*true,\\s*false\\s*\\]'],
+    ['arrayOfRanges', ['1', '5', '10'], '\\[\\s*1,\\s*5,\\s*10\\s*\\]'],
+  ])('setting of type `%s` with input "%s" sets to correct value successfully`', async (type, value, expectation) => {
+    const args = ['!settings', 'test', type, ...value];
+
+    await settingsCommand.run(MOCK_BOT, makeMockMessage(args), args);
+    expect(MOCK_BOT.message.reply).toHaveBeenCalledWith(expect.stringMatching(new RegExp(`"${type}":\\s*${expectation}`)));
+  });
+
+  test.each([
+    ['string', ['10']],
+    ['number', ['foo']],
+    ['boolean', ['not-boolean']],
+    ['arrayOfStrings', ['1', '2', '3']],
+    ['arrayOfNumbers', ['not', 'numbers']],
+    ['arrayOfBooleans', ['not', 'booleans']],
+    ['arrayOfRanges', ['not', 'in', 'range']],
+    ['arrayOfRanges', ['-1']],
+  ])('setting of type `%s` with input "%s" fails expectedly', async (type, value) => {
+    const args = ['!settings', 'test', type, ...value];
+
+    await settingsCommand.run(MOCK_BOT, makeMockMessage(args), args);
+    expect(MOCK_BOT.message.reply).toHaveBeenCalledWith(
+      expect.stringMatching(`\`${value}\` is not a valid value. Expected type of \`${testServerSettings.test[type].type}\``),
+    );
   });
 });
